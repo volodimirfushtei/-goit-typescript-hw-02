96929146387a321476af051137c67d6c2741b71e
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-10-31 16:40:25 +0200

    add

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,5 +1,6 @@
 import s from "./LoadMoreBtn.module.css";
 import Loader from "../Loader/Loader";
+import toast, { Toaster } from "react-hot-toast";
 
 interface LoadMoreBtnProps {
   haveImages: number;
@@ -13,17 +14,27 @@ const LoadMoreBtn: React.FC<LoadMoreBtnProps> = ({
   onClick,
 }) => {
   if (haveImages <= 0) {
+    if (haveImages === 0) {
+      toast.error("No more images to load", { duration: 3000 });
+    }
     return null;
   }
+
   return (
     <div className={s.container_button_loadmore}>
       {isLoadingMore ? (
         <Loader />
       ) : (
-        <button className={s.button_loadmore} type="button" onClick={onClick}>
+        <button
+          className={s.button_loadmore}
+          type="button"
+          onClick={onClick}
+          disabled={isLoadingMore}
+        >
           Load more
         </button>
       )}
+      <Toaster />
     </div>
   );
 };
